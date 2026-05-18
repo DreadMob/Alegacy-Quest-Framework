c59ff7d17af6573ececf56fd0f1a3a61ee323c92
DreadMob <shadows7knight@gmail.com>
2026-05-19 05:23:35 +0700

    convert config-editor to submodule (DreadMob/Alegacy-QF-Editor)

--- config-editor/rebuild.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
-set PATH=%USERPROFILE%\.cargo\bin;%PATH%
-set LIB=%LIB%;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\um\x64;C:\Program Files (x86)\Windows Kits\10\Lib\10.0.26100.0\ucrt\x64
-set INCLUDE=%INCLUDE%;C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\um;C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\ucrt;C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\shared
-cd /d "c:\Users\shado\Desktop\vsquest\config-editor-tauri\src-tauri"
-cargo build
